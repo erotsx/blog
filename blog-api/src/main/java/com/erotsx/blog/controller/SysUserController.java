b7042fe434d0cba20e87352d4689d96980b1ea29
erotsx <erotsx@gmail.com>
2022-06-23 21:00:20 +0800

    图床工具类

--- blog-api/src/main/java/com/erotsx/blog/controller/SysUserController.java
+++ blog-api/src/main/java/com/erotsx/blog/controller/SysUserController.java
@@ -1,13 +1,14 @@
 package com.erotsx.blog.controller;
 
 import com.erotsx.blog.service.SysUserService;
+import com.erotsx.blog.utils.ImgBedUtils;
 import com.erotsx.blog.vo.Result;
 import com.erotsx.blog.vo.SysUserVo;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("user")
@@ -23,5 +24,12 @@ public class SysUserController {
     @RequestMapping("getUserInfo")
     public Result<SysUserVo> getUserInfo(@RequestHeader("Authorization") String token) {
         return Result.success(sysUserService.getUserInfo(token));
+    }
+
+    @PutMapping("updateAvatar")
+    public Result<String> updateAvatar(@RequestBody MultipartFile file) throws IOException {
+        String msg = ImgBedUtils.upload(file);
+        System.out.println();
+        return Result.success(msg);
     }
 }
--- blog-api/src/main/java/com/erotsx/blog/utils/ImgBedUtils.java
+++ blog-api/src/main/java/com/erotsx/blog/utils/ImgBedUtils.java
@@ -1,4 +1,63 @@
 package com.erotsx.blog.utils;
 
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "img-bed")
 public class ImgBedUtils {
+
+    private static final String IMG_URL_API = "https://www.imgurl.org/api/v2/upload";
+    private static String UID;
+    private static String Token;
+
+
+    /**
+     * 上传图片到图床
+     *
+     * @param file 要上传的图片
+     * @return 图片地址
+     */
+    public static String upload(MultipartFile file) throws IOException {
+        ByteArrayResource fileAsResource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+
+            @Override
+            public long contentLength() {
+                return file.getSize();
+            }
+        };
+        MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("file", fileAsResource);
+        map.add("uid", UID);
+        map.add("token", Token);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(IMG_URL_API, map, String.class);
+        System.out.println(responseEntity.getBody());
+        return responseEntity.getBody();
+    }
+
+    public void setUID(String UID) {
+        ImgBedUtils.UID = UID;
+    }
+
+    public void setToken(String Token) {
+        ImgBedUtils.Token = Token;
+    }
+
 }
