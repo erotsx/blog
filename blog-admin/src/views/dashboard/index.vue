189a4a8ffbcef693a8d8f9dbeff191265c4d1d6f
erotsx <erotsx@gmail.com>
2022-07-30 23:26:26 +0800

    仪表盘

--- blog-admin/src/views/dashboard/index.vue
+++ blog-admin/src/views/dashboard/index.vue
@@ -1,6 +1,51 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <el-row :gutter="30">
+      <el-col :span="6">
+        <el-card>
+          <div class="card-icon-container">
+            <i class="iconfont el-icon-myfangwenliang" style="color:#40C9C6" />
+          </div>
+          <div class="card-desc">
+            <div class="card-title">访问量</div>
+            <div class="card-count">{{ viewsCount }}</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <div class="card-icon-container">
+            <i class="iconfont el-icon-myuser" style="color:#34BFA3" />
+          </div>
+          <div class="card-desc">
+            <div class="card-title">用户量</div>
+            <div class="card-count">{{ userCount }}</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <div class="card-icon-container">
+            <i class="iconfont el-icon-mywenzhang-copy" style="color:#F4516C" />
+          </div>
+          <div class="card-desc">
+            <div class="card-title">文章量</div>
+            <div class="card-count">{{ articleCount }}</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <div class="card-icon-container">
+            <i class="el-icon-s-comment" style="color:#36A3F7" />
+          </div>
+          <div class="card-desc">
+            <div class="card-title">留言量</div>
+            <div class="card-count">{{ messageCount }}</div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -13,11 +58,32 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  created() {
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.card-icon-container {
+  display: inline-block;
+  font-size: 3rem;
+}
+.card-desc {
+  font-weight: bold;
+  float: right;
+}
+.card-title {
+  margin-top: 0.3rem;
+  line-height: 18px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 1rem;
+}
+.card-count {
+  margin-top: 0.75rem;
+  color: #666;
+  font-size: 1.25rem;
+}
 .dashboard {
   &-container {
     margin: 30px;
